f95fc9508e6b8fa33d2fbc8bcfd7c01899833a44
Hari Yerramsetty <yhvardhan@gmail.com>
2018-04-24 11:43:16 -0400

    Adding filter to home controller for user authentication

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_home, only: [:show, :edit, :update, :destroy]
+  
 
   # GET /homes
   # GET /homes.json
