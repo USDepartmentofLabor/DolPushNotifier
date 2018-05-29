67d5014bdc671a168e9356f3c97ba4e0a7926c3e
Hari Yerramsetty <yhvardhan@gmail.com>
2018-05-29 14:56:51 -0400

    Adding 1 Admin user accounts to create initial admin accounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+# Create Admin Users
+User.create(email: "ensadmin@dol.gov", password: "ujn0912~", is_admin: true)
