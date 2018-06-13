26f346623ae87af8b573f61149475eab4b70c2be
Hari Yerramsetty <yhvardhan@gmail.com>
2018-06-13 15:31:20 -0400

    ENS-19 Add password complexity
    1. Add a regex validator to check if password meets security
    requirements
    2. The password should be minimum of 8 charecters and must have one
    uppercase, one lowercase, one digit and one special charecter
    3. Add model validations for email uniqueness and presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :registerable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  validates :email, uniqueness: true, presence: true
+  validate :password_complexity
+  
+  def password_complexity
+    return if password.blank? || password =~ /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/
+
+    errors.add :password, 'Password requirements not met, length should be minimum 8 charecters and include: 1 Uppercase, 1 Lowercase, 1 Digit and 1 special charecter'
+  end
 end
