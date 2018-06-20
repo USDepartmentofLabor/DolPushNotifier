311c71898eefbeecb970d2fb4c5ca3cd98d1e47d
Giridhar Bandi <bandi.giridhar.r@dol.gov>
2018-06-20 09:51:36 -0400

    Style changes to alert messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,10 @@
 module ApplicationHelper
   def flash_class(level)
-    Rails.logger.debug("***** #{level} #{level.class}")
     case level
         when "notice" then "alert alert-info"
         when "success" then "alert alert-success"
-        when "error" then "alert alert-error"
-        when "alert" then "alert alert-error"
+        when "error" then "alert alert-danger"
+        when "alert" then "alert alert-warning"
     end
   end
 end
